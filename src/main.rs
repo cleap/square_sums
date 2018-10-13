8c098c8cc938ac6c8fee7b2ed4f3e0c2b6672c37
cleap <cleapdev@gmail.com>
2018-10-12 19:14:07 -0600

    Made rendering more object-oriented

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,44 @@ impl Node {
         }
     }
 
+    fn draw(&mut self, tot_num: u32, radius: f64, context: &Context,
+            graphics: &mut G2d) {
+        let [x, y] = coord_from_n(self.num-1, tot_num, radius);
+        ellipse(
+            [1.0, 0.0, 0.0, 1.0],
+            ellipse::circle(x, y, 5.0),
+            context.transform,
+            graphics
+        );
+    }
+
+    fn draw_text(&mut self, tot_num: u32, radius: f64, context: &Context,
+            graphics: &mut G2d, glyphs: &mut Glyphs) {
+
+        let [x, y] = coord_from_n(self.num-1, tot_num, radius);
+        text::Text::new_color([0.0, 0.0, 0.0, 1.0], 16).draw(
+            &format!("{:02}", self.num),
+            glyphs,
+            &context.draw_state,
+            context.transform.trans(x - 12.5, y + 6.0),
+            graphics
+        );
+    }
+
+    fn draw_link(n1: u32, n2: u32, tot_num: u32, radius: f64,
+            context: &Context, graphics: &mut G2d) {
+
+        let [x1, y1] = coord_from_n(n1 - 1, tot_num, radius);
+        let [x2, y2] = coord_from_n(n2 - 1, tot_num, radius);
+        line(
+            [1.0, 0.0, 0.0, 1.0],
+            1.0,
+            [x1, y1, x2, y2],
+            context.transform,
+            graphics
+         );
+    }
+
     fn generate_graph(number: u32) -> Vec<Node> {
 
         // Initialize nodes
@@ -66,12 +104,11 @@ fn main() {
     // Initialize text stuff
     let ref font = "fonts/FreeMono.ttf";
     let factory = window.factory.clone();
-    let mut glyphs = Glyphs::new(font, factory, TextureSettings::new()).unwrap();
+    let mut glyphs = Glyphs::new(font, factory,
+        TextureSettings::new()).unwrap();
 
     // Other vars
     let mut number: u32 = 15;
-
-    // Initialize nodes
     let mut nodes: Vec<Node> = Node::generate_graph(number);
 
     // Event handler
@@ -80,15 +117,16 @@ fn main() {
 
         // Key events
         if let Some(Button::Keyboard(key)) = e.press_args() {
-            if key == Key::Space || key == Key::Right || key == Key::Up {
-                number += 1;
-                nodes = Node::generate_graph(number);
-            }
-            else if key == Key::Backspace || key == Key::Left || key == Key::Down {
-                if number > 0 {
-                    number -= 1;
-                    nodes = Node::generate_graph(number);
-                }
+            match key {
+                Key::Right | Key::Up => {
+                        number += 1;
+                        nodes = Node::generate_graph(number);
+                    },
+                Key::Left | Key::Down => {
+                        number -= 1;
+                        nodes = Node::generate_graph(number);
+                    }
+                _ => {}
             }
         }
 
@@ -97,36 +135,16 @@ fn main() {
 
             let radius: f64 = 8.0 * number as f64;
 
-            window.draw_2d(&e, |context, graphics| {
+            window.draw_2d(&e, |context, mut graphics| {
 
                 clear([1.0, 1.0, 1.0, 1.0], graphics);
 
-                for node in &nodes {
-                    let [x1, y1] = coord_from_n(node.num-1, number, radius);
-                    ellipse(
-                        [1.0, 0.0, 0.0, 1.0],
-                        ellipse::circle(x1, y1, 5.0),
-                        context.transform,
-                        graphics
-                    );
+                for node in &mut nodes {
+                    node.draw(number, radius, &context, &mut graphics);
+                    node.draw_text(number, radius + 25.0, &context, &mut graphics, &mut glyphs);
                     for other in &node.links {
-                        let [x2, y2] = coord_from_n(*other-1, number, radius);
-                        line(
-                            [1.0, 0.0, 0.0, 1.0],
-                            1.0,
-                            [x1, y1, x2, y2],
-                            context.transform,
-                            graphics
-                         );
+                        Node::draw_link(node.num, *other, number, radius, &context, &mut graphics)
                     }
-                    let [x1, y1] = coord_from_n(node.num-1, number, radius + 25.0);
-                    text::Text::new_color([0.0, 0.0, 0.0, 1.0], 16).draw(
-                        &format!("{:02}", node.num),
-                        &mut glyphs,
-                        &context.draw_state,
-                        context.transform.trans(x1 - 12.5, y1 + 6.0),
-                        graphics
-                    );
                 }
             });
         }
