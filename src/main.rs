ef1632cc94f3823056c8c72d7c7addc8a4807e1b
cleap <cleapdev@gmail.com>
2018-10-12 18:24:34 -0600

    Nodes are now structs

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,55 @@ use piston_window::*;
 const WIDTH: u32 = 1000;
 const HEIGHT: u32 = WIDTH;
 
+struct Node {
+    num: u32,
+    links: Vec<u32>
+}
+
+impl Node {
+
+    fn new(num: u32) -> Node {
+        Node {
+            num,
+            links: Vec::new()
+        }
+    }
+
+    fn connect(&mut self, other: u32) {
+        // If the nodes sum to a square
+        if  self.num != other &&
+                ((self.num + other) as f64).sqrt().fract() == 0.0 &&
+                self.num + other != 4 { // Because Elijah said so
+            self.links.push(other);
+            println!("Connecting {:02} and {:02}", self.num, other);
+        }
+    }
+
+    fn generate_graph(number: u32) -> Vec<Node> {
+
+        // Initialize nodes
+        let mut nodes: Vec<Node> = Vec::new();
+        for n in 1..number+1 {
+            nodes.push(Node::new(n));
+        }
+
+        // Connect graph
+        for node in &mut nodes {
+            for other in 1..number+1 {
+                node.connect(other);
+            }
+        }
+
+        nodes
+    }
+}
+
 // Generates the coordinates for a number in the list
 fn coord_from_n(n: u32, maxn: u32, radius: f64) -> [f64; 2] {
     let scale: f64 = 2.0*std::f64::consts::PI / (maxn as f64);
     let xcoord: f64 = (n as f64 * scale).sin() * radius + WIDTH as f64 / 2.0;
     let ycoord: f64 = -(n as f64 * scale).cos() * radius + HEIGHT as f64 / 2.0;
     [xcoord, ycoord]
-}
-
-// Returns true if a link exists between the two points, false otherwise
-// (except for 1 and 3)
-fn is_link(n1: u32, n2: u32) -> bool {
-    ((n1 + n2) as f64).sqrt().fract() == 0.0 && (n1 + n2) > 4 // Because Elijah said so
 }
 
 fn main() {
@@ -34,6 +71,9 @@ fn main() {
     // Other vars
     let mut number: u32 = 15;
 
+    // Initialize nodes
+    let mut nodes: Vec<Node> = Node::generate_graph(number);
+
     // Event handler
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
@@ -42,10 +82,12 @@ fn main() {
         if let Some(Button::Keyboard(key)) = e.press_args() {
             if key == Key::Space || key == Key::Right || key == Key::Up {
                 number += 1;
+                nodes = Node::generate_graph(number);
             }
             else if key == Key::Backspace || key == Key::Left || key == Key::Down {
                 if number > 0 {
                     number -= 1;
+                    nodes = Node::generate_graph(number);
                 }
             }
         }
@@ -59,39 +101,32 @@ fn main() {
 
                 clear([1.0, 1.0, 1.0, 1.0], graphics);
 
-                // Draw Nodes
-                for n in 0..number {
-                    let [xcoord, ycoord] = coord_from_n(n, number, radius);
+                for node in &nodes {
+                    let [x1, y1] = coord_from_n(node.num-1, number, radius);
                     ellipse(
                         [1.0, 0.0, 0.0, 1.0],
-                        ellipse::circle(xcoord, ycoord, 5.0),
+                        ellipse::circle(x1, y1, 5.0),
                         context.transform,
                         graphics
                     );
-                    let [xcoord, ycoord] = coord_from_n(n, number, radius + 25.0);
+                    for other in &node.links {
+                        let [x2, y2] = coord_from_n(*other-1, number, radius);
+                        line(
+                            [1.0, 0.0, 0.0, 1.0],
+                            1.0,
+                            [x1, y1, x2, y2],
+                            context.transform,
+                            graphics
+                         );
+                    }
+                    let [x1, y1] = coord_from_n(node.num-1, number, radius + 25.0);
                     text::Text::new_color([0.0, 0.0, 0.0, 1.0], 16).draw(
-                        &format!("{:02}", n+1),
+                        &format!("{:02}", node.num),
                         &mut glyphs,
                         &context.draw_state,
-                        context.transform.trans(xcoord - 12.5, ycoord + 6.0),
+                        context.transform.trans(x1 - 12.5, y1 + 6.0),
                         graphics
                     );
-                }
-
-                // Draw Links
-                for n1 in 0..number {
-                    for n2 in n1+1..number {
-                        if is_link(n1+1, n2+1) {
-                            let [x1, y1] = coord_from_n(n1, number, radius);
-                            let [x2, y2] = coord_from_n(n2, number, radius);
-                            line([1.0, 0.0, 0.0, 1.0],
-                                 1.0,
-                                 [x1, y1, x2, y2],
-                                 context.transform,
-                                 graphics
-                             );
-                        }
-                    }
                 }
             });
         }
